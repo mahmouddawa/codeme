5de9b036366cfc7abcefbd7fa09bfe467b4f5fad
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-03-26 12:05:20 +0100

    caching to indexedDB
    adding the localforage library and use it to store the cached data from the modules in the indexedDB(which is a browser based DB)

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,21 +1,32 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const fileCache = localforage.createInstance({
+  name : 'fileCache'
+});
+
+
+export const unpkgPathPlugin = (inputCode:string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // handle root entry file of index.js
+      build.onResolve({filter:/(^index\.js$)/},()=>{
+        return {path: 'index.js', namespace: 'a'}
+      });
+      // this will search for the ./ and the ../
+      // handle relative paths in a module
+      build.onResolve({filter:/^\.+\//}, (args:any)=>{
+        return {
+          path: new URL(args.path,
+             'https://unpkg.com' + args.resolveDir + '/').href,
+              namespace: 'a'
+        }
+      })
+      // handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => { // overwrite the path that esbuild use to read the file "normally the file system on my machine"
-        console.log('onResolve', args);
-        if(args.path === 'index.js'){
-          return { path: args.path, namespace: 'a' };
-        }
-        else if (args.path.includes('./' || args.path.includes('../'))){
-          return {
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-            namespace: 'a'
-          }
-        }
+
           return {
             path: `https://unpkg.com/${args.path}`,
             namespace: 'a'
@@ -29,20 +40,26 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',   
-            contents: `
-              import React, {useState} from 'react';
-              import ReactDOM from 'react-dom';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         } 
+        
+        //check if we already fetched this file and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // if it is the return it immediately
+        if(cachedResult){
+          return cachedResult;
+        }
+        //if it is not in the cache
         const {data, request} = await axios.get(args.path);
-
-        return {
+        const result:esbuild.OnLoadResult =  {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./',request.responseURL).pathname
         }
+        // store it in the cahe 
+        await fileCache.setItem(args.path,result);
+        return result;
       });
     },
   };
