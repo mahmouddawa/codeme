834ea5ace136b1b92b0422b94b84835e8b3983f9
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-06-21 22:50:54 +0200

    refactoring

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,41 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name : 'fileCache'
+});
+
+
+export const fetchPlugin = (inputCode : string)=>{
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild){
+    build.onLoad({ filter: /.*/ }, async (args: any) => { // overwrite the load logic to not take the content from the file systems
+      if (args.path === 'index.js') {
+        return {
+          loader: 'jsx',   
+          contents: inputCode,
+        };
+      } 
+      
+      //check if we already fetched this file and if it is in the cache
+      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+      // if it is the return it immediately
+      if(cachedResult){
+        return cachedResult;
+      }
+      //if it is not in the cache
+      const {data, request} = await axios.get(args.path);
+      const result:esbuild.OnLoadResult =  {
+        loader: 'jsx',
+        contents: data,
+        resolveDir: new URL('./',request.responseURL).pathname
+      }
+      // store it in the cahe 
+      await fileCache.setItem(args.path,result);
+      return result;
+    });
+    }
+  }
+} 
